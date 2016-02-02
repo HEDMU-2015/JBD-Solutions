a122d719c1802afcdffac7d59872a71c3ad81ab5
jeppsc11 <jeppespanggaard11@gmail.com>
2016-02-02 11:15:43 +0100

    Virker igen

--- Firstthread/src/Main.java
+++ Firstthread/src/Main.java
@@ -4,11 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		VorThread vt = new VorThread();
 		vt.start();
-<<<<<<< HEAD
-		int x =1;
-=======
 		int x =15;
->>>>>>> branch 'master' of https://github.com/HEDMU-2015/JBD-Solutions-IPSOS.git
 		System.out.println(x);
 	}
 }
