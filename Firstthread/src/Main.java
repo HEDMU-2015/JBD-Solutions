6d8223497fea0c08a7e36de754af0d47ac052bd2
Brian Laursen <bl_charlie@hotmail.com>
2016-02-02 10:27:25 +0100

    fisse

--- Firstthread/src/Main.java
+++ Firstthread/src/Main.java
@@ -4,7 +4,15 @@ public class Main {
 	public static void main(String[] args) {
 		VorThread vt = new VorThread();
 		vt.start();
-		int x =9;
+		int x =98;
+		
+		saf
+		as
+		fa
+		sf
+		as
+		fs
+		af
 	}
 
 }
