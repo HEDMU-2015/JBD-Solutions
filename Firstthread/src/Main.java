d8e185d85ba251dfd804115a0a31902ff90bed8e
Brian Laursen <bl_charlie@hotmail.com>
2016-02-02 10:29:44 +0100

    Revert "fisse"
    This reverts commit 6d8223497fea0c08a7e36de754af0d47ac052bd2.

--- Firstthread/src/Main.java
+++ Firstthread/src/Main.java
@@ -4,15 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		VorThread vt = new VorThread();
 		vt.start();
-		int x =98;
-		
-		saf
-		as
-		fa
-		sf
-		as
-		fs
-		af
+		int x =9;
 	}
 
 }
